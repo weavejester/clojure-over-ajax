2c47827ae47ba60c31688323a353ff6fd5f929ee
weavejester <jreeves@weavejester.com>
2009-01-27 23:42:51 +0000

    REPL connected up, sending AJAX back and forth

--- ajax_repl.clj
+++ ajax_repl.clj
@@ -1,25 +1,38 @@
 (ns ajax-repl
   (:use compojure))
 
+(def repl-scripts
+  (include-js "/js/jquery.js"
+              "/js/mouseapp_2.js"
+              "/js/mouserepl_2.js"
+              "/js/repl.js"))
+
+(def credits
+  [:p "Based on code from _why's "
+    [:q (link-to "http://tryruby.hobix.com/" "Try Ruby!")]])
+
 (defn show-repl
-  "Function to create HTML boilerplate."
+  "HTML for the REPL index page"
   []
   (html
     [:html
       [:head
-        [:title "Clojure REPL"]]
+        [:title "Clojure REPL"]
+        repl-scripts]
       [:body
-        [:div#terminal]]]))
+        [:div#terminal
+          [:div#repl]]
+        credits]]))
 
 (defn eval-repl-cmd
   "Evaluate a command and return the result."
   [cmd]
-  "TODO")
+  (str "Run command: " cmd))
 
 (defservlet repl-servlet
   (GET "/"
     (show-repl))
-  (POST "/repl"
+  (GET "/repl"
     (eval-repl-cmd (params :cmd)))
   (GET "/*"
     (or (serve-file (route :*))
